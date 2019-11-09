50c97af5f3fa642cff425d40a93cd3a29df2be69
Joe Marino <joeycmarino@gmail.com>
2019-11-09 15:40:09 -0500

    Updates

---
+++ precache-manifest.5ad18270346281b8a2ea03d6f29cd512.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1c931dc61d18678264323cff71a0fc13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5ccf526292f5588e9b46",
+    "url": "/static/css/main.e46a9304.chunk.css"
+  },
+  {
+    "revision": "086c0d8ba6ff9a020e5b",
+    "url": "/static/js/2.b33c12be.chunk.js"
+  },
+  {
+    "revision": "5ccf526292f5588e9b46",
+    "url": "/static/js/main.72f177db.chunk.js"
+  },
+  {
+    "revision": "f891120763f09722d648",
+    "url": "/static/js/runtime-main.5d3069f4.js"
+  }
+]);
